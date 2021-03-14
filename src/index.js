4a1e994f90474e7291ea9c7d37c1b93e6581a062
Insikynwa <pavel.churkin@mail.ru>
2021-03-14 23:50:04 +0300

    feat: implement function toReadable that converts given number, to readable stringment function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-module.exports = function toReadable (number) {
-  
-}
+const a = [
+    " ",
+    "one ",
+    "two ",
+    "three ",
+    "four ",
+    "five ",
+    "six ",
+    "seven ",
+    "eight ",
+    "nine ",
+    "ten ",
+    "eleven ",
+    "twelve ",
+    "thirteen ",
+    "fourteen ",
+    "fifteen ",
+    "sixteen ",
+    "seventeen ",
+    "eighteen ",
+    "nineteen ",
+];
+const b = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+];
+
+module.exports = function toReadable(number) {
+    if (number == 0) {
+        return "zero";
+    } else if ((number = number.toString()).length > 9) return "overflow";
+    let n = ("000000000" + number)
+        .substr(-9)
+        .match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return;
+    let str = "";
+    str += n[1] != 0 ? a[Number(n[1])] || b[n[1][0]] + " " + a[n[1][1]] : "";
+    str += n[2] != 0 ? a[Number(n[2])] || b[n[2][0]] + " " + a[n[2][1]] : "";
+    str += n[3] != 0 ? a[Number(n[3])] || b[n[3][0]] + " " + a[n[3][1]] : "";
+    str +=
+        n[4] != 0
+            ? (a[Number(n[4])] || b[n[4][0]] + " " + a[n[4][1]]) + "hundred "
+            : "";
+    str += n[5] != 0 ? a[Number(n[5])] || b[n[5][0]] + " " + a[n[5][1]] : "";
+    return str.trimEnd();
+};
